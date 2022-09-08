0911eac8ece80c3b50b970ad5f5c956bd78b5a7c
Luis Balcaza <luisratoncitobz_3108@hotmail.com>
2022-09-08 15:40:30 -0500

    cree un programa en c++

--- sumas.cpp
+++ sumas.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
-int main(){
+int mxin(){
 float x,y,z;
-cout<<"ingrese 2 numeros x y:";
+cout<<"Ingrese 2 numeros sepxrxdos de espxzio x y y:";
 cin>>x>>y;
 z=x+y;
-cout<<"El resultado es:";
+cout<<"el resultxdo es:";
 cout<<z<<endl;
 return 0;
 }
